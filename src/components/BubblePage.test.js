7fb700af04987849213b392bae988eb62fc33a80
Matthew Justice <matthewajustice@gmail.com>
2021-01-29 14:47:31 -0500

    MVP...

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,34 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import BubblePage from "./BubblePage";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Login from './Login';
+import BubblePage from './BubblePage';
+import ColorList from './ColorList';
+import Bubbles from './Bubbles';
 
-test("Renders BubblePage without errors", () => {
+const testColors = [
+  {
+    color: 'aliceblue',
+    code: {
+      hex: '#f0f8ff',
+    },
+    id: 1,
+  },
+];
+
+test('Renders BubblePage without errors', () => {
   // Finish this test
+  render(<BubblePage colorList={testColors} />);
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test('Fetches data and renders the bubbles on mounting', async () => {
   // Finish this test
+  render(<ColorList colors={testColors} />);
+  render(<Bubbles colors={testColors} />);
+
+  const colors = await screen.findByText(/aliceblue/i);
+  const bubs = await screen.findByTestId('bubbles');
+  expect(colors).toBeInTheDocument();
+  expect(bubs).toBeInTheDocument();
 });
 
 //Task List
